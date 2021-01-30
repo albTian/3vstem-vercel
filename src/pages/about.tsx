2d03b38f292563e949bf1cd2ca5c7b7786ec0545
Albert Tian <alberttian@alberts-air.lan>
2021-01-29 23:50:34 -0500

    initial vercel test...

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default function AboutPage(props) {
+    return (
+        <>
+            <Link to="/">GO BACK</Link>
+        </>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from 'gatsby'
+
+export default function Home() {
+  return (
+    <>
+      <div>IM CHANGING!</div>
+      <Link to="about">to about</Link>
+    </>
+  )
+}
